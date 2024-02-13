0fa825996a9ba80b449b2b3bfc8c542b53d6994c
davidwrossiter <davewrossiter@gmail.com>
2024-02-13 19:41:50 +0000

    adjusted the header to change text and icon colour

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
 const changeBg = () => {
   let header = document.getElementById('header');
+
   let scrollValue = window.scrollY;
   console.log(scrollValue);
-  if(scrollValue > 400) {
+  if(scrollValue > 563) {
     header.classList.add('transition-all');
     header.classList.add('bg-white');
+    document.getElementById('header-mobile-image-white').classList.add('hidden')
+    document.getElementById('header-mobile-image-black').classList.remove('hidden')
+    document.getElementById('header-text').classList.add('text-black')
+
   } else {
     header.classList.remove('bg-white')
+    document.getElementById('header-mobile-image-black').classList.add('hidden')
+    document.getElementById('header-mobile-image-white').classList.remove('hidden')
+    document.getElementById('header-text').classList.remove('text-black')
+
   };
 }
 
